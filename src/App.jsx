4f4e9e98e67afa3123eedbb96f8edbd3ecc8ad08
chavdawala <harshchavda529@gmail.com>
2025-01-06 17:23:36 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Signup from './components/Signup';
 import Dashboard from './components/Dashboard';
+import Index from './components/Index';
+import Department from './components/Department';
+import Fullstack from './components/Fullstack';
+import Dataana from './components/Dataana';
+import Hr from './components/Hr';
+import Logout from './components/Logout';
+import Employee from './components/Employee';
 
 const App = () => {
   return (
@@ -11,8 +18,13 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        {/* <Route path="/" element={Home} />  */}
-       
+        <Route path="/index" element={<Index />} />
+        <Route path="/department" element={<Department />} />
+        <Route path="/fullstack" element={<Fullstack />} />
+        <Route path="/dataana" element={<Dataana />} />
+        <Route path="/hr" element={<Hr />} /> {/* Route is fine here */}
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/employee" element={<Employee />} /> {/* Corrected route */}
       </Routes>
     </Router>
   );
