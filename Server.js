11136b270fd32ff3b3982244a98df35342e834b4
chavdawala <harshchavda529@gmail.com>
2025-03-01 11:48:35 +0530

    cors

--- Server.js
+++ Server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 // CORS Configuration
 app.use(cors({
-    origin: ["http://localhost:5173"],  
+    origin: ["http://localhost:5173", "attendance-flf3-git-main-chavdawalas-projects.vercel.app", "attendance-flf3.vercel.app"],  
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization", "Accept"]
