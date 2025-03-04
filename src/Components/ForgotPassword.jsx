28f8fff705a1d79b6d2c809c5dda57178515e365
chavdawala <harshchavda529@gmail.com>
2025-03-04 12:41:33 +0530

    link

--- src/Components/ForgotPassword.jsx
+++ src/Components/ForgotPassword.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { useState } from 'react';
 import { Mail } from "lucide-react";
+import { Link } from 'react-router-dom';
 
 const ForgotPassword =() => {
     const [email, setEmail] = useState('');
